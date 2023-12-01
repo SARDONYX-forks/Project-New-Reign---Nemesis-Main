7d9cd4a27419145d30650161fe319f3b6a89cf87
SARDONYX <68905624+SARDONYX-sard@users.noreply.github.com>
2023-12-02 01:46:19 +0900

    ci(xmake): change to x64

--- src/xmake.lua
+++ src/xmake.lua
@@ -2,7 +2,7 @@ target("NemesisUnlimitedBehaviorEngine")
     add_files("**.cpp")
     add_files("../resources/*.rc", "../resources/*.qrc")
     add_frameworks("QtGui", "QtWidgets") -- "QtCore" is default
-    add_includedirs("../include", "../external","../Python3_8_10_x86-windows/Python38-32/include")
-    add_linkdirs("../Python3_8_10_x86-windows/Python38-32/libs")
-    add_packages("boost", "qt5")
+    add_includedirs("../include", "../external","../Python3_10_11_x64-windows/3.10.11/include")
+    add_linkdirs("../Python3_10_11_x64-windows/3.10.11/libs")
+    add_packages("boost")
     add_rules("qt.application") -- ref: https://tboox.org/2018/05/30/support-qt/#:~:text=Quick%20Application%20Program
--- xmake.lua
+++ xmake.lua
@@ -6,11 +6,11 @@ set_version("0.84")
 set_warnings("allextra")
 
 -- allowed
-set_allowedarchs("x86")
+set_allowedarchs("x64") -- No need build hkxcmd, so we can use x64
 set_allowedmodes("debug", "releasedbg")
 
 -- default
-set_defaultarchs("x86") -- For hkxcmd
+set_defaultarchs("x64")
 set_defaultmode("releasedbg")
 
 -- rules
@@ -22,6 +22,5 @@ set_policy("package.requires_lock", true)
 -- packages
 -- ref: https://xmake.io/mirror/manual/global_interfaces.html#:~:text=Install%20third%2Dparty%20manager%20package
 add_requires("conan::boost/1.72.0", { alias = "boost" }) -- https://conan.io/center/recipes/boost
-add_requires("conan::qt/5.15.11", { alias = "qt5" }) -- https://conan.io/center/recipes/qt
 
 includes("src")

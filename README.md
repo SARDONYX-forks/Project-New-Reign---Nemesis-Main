22c8429ad22eac5f7305b2bbf966ceab4083be0b
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-12-14 03:36:34 +0900

    feat!: implement local build batch

--- README.md
+++ README.md
@@ -16,20 +16,8 @@ Thanks to all for supporting this project
     - To avoid this error
       [cannot open file 'libffi-7.lib'](https://discuss.python.org/t/struggling-to-build-python-on-windows-10/11031)
 
-1.Edit `CMakeLists.txt` when Local build
-
-```cmake
-option(UseLocalQt "Uses local copy of Qt instead of GitHub Action" ON) # OFF -> ON
-set(Qt5_DIR "${PROJECT_SOURCE_DIR}/build/5.14.1/msvc2017/lib/cmake/Qt5" CACHE PATH "Qt local path")
-```
-
-2.Execute the following PowerShell command.
+Execute the following PowerShell command.
 
 ```powershell
-New-Item -ItemType Directory ./build -ErrorAction SilentlyContinue
-Set-Location ./build
-pip install aqt
-aqt install-qt windows desktop 5.14.2 win32_msvc2017
-cmake -A Win32 ..
-cmake --build . -j $Env:NUMBER_OF_PROCESSORS --config Release
+./build.ps1
 ```
---
+++ build.ps1
@@ -0,0 +1,20 @@
+New-Item -ItemType Directory "$(Get-Location)/build" -ErrorAction SilentlyContinue
+
+#! NOTE: VS2022 gives me a syntax error when I try to compile 5.14.2. Therefore, use the one for VS2019.
+# If GitHub CI definition, then ignore.
+if (!$env:Qt5_DIR) {
+  $env:Qt5_DIR = Join-Path -Path $(Get-Location) -ChildPath "build/5.15.2/msvc2019/lib/cmake"
+  if (!$(Test-Path $env:Qt5_DIR)) {
+    Write-Host "Not found aqt(Qt install command). Try to install with Python3..." -ForegroundColor Cyan
+    if (Get-Command rye -ErrorAction SilentlyContinue) { rye install apt }
+    elseif (Get-Command python3 -ErrorAction SilentlyContinue) { pip install aqt }
+    else { throw "Couldn't found Python3" }
+    aqt install-qt windows desktop 5.15.2 win32_msvc2019
+  }
+}
+
+# -S: source path
+# -B: build path
+# -A Win32: Need IA-32(32bit) mode for `hkxcmd`
+cmake -S . -B ./build -A Win32 -D CMAKE_BUILD_TYPE=Release
+cmake --build ./build -j $Env:NUMBER_OF_PROCESSORS --config Release

f8de34514e2d3a75f90999f8bed386aa4e436a7e
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2026-02-20 00:17:35 +0900

    feat: try to compile multi os

--- xmake/rules/link_archive_all.lua
+++ xmake/rules/link_archive_all.lua
@@ -1,29 +1,15 @@
 -- NOTE: Prevent `Nemesis_Havok` from exhibiting unintended behavior during optimization
-rule("link_archive_all", function ()
-    on_config(function (target)
+rule("link_archive_all", function()
+    on_config(function(target)
         local lib_file = path.absolute(target:targetfile()) -- .lib file path
-        -- local depfile = path.absolute(target:dep("Nemesis_Havok_static"):targetfile())
 
         if target:is_plat("windows") then
             target:add("cxflags", "/bigobj")
-            target:add("ldflags",
-                "/WHOLEARCHIVE:" .. lib_file,
-                { force = true }
-            )
-
+            target:add("ldflags", "/WHOLEARCHIVE:" .. lib_file, { force = true })
         elseif target:is_plat("macosx") then
-            target:add("ldflags",
-                "-Wl,-force_load," .. lib_file,
-                { force = true }
-            )
-
+            target:add("ldflags", "-Wl,-force_load," .. lib_file, { force = true })
         elseif target:is_plat("linux") then
-            target:add("ldflags",
-                "-Wl,--whole-archive",
-                lib_file,
-                "--no-whole-archive",
-                { force = true }
-            )
+            target:add("ldflags", "-Wl,--whole-archive," .. lib_file .. ",--no-whole-archive", { force = true })
         end
     end)
 end)
